eb70c272bd529f8dba01aeed49af1e387f3700ca
Perseusxe <perseus20061103@gmail.com>
2024-11-27 18:05:29 +0800

    mdku

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-   <div style={{height: "100px", width: "500px", border: "1px solid white"}}>
-   </div>
-   <div style={{height: "100px", width: "500px", border: "1px solid white"}}>
-   </div>
-   <div style={{height: "100px", width: "500px", border: "1px solid white"}}>
-   </div>
-   <div style={{height: "100px", width: "500px", border: "1px solid white"}}>
-   </div>
-   </div>
+      <div></div>
+    </div>
   );
 }
